136693f3fb21a79cafa5ce78a3cc611450973756
Dương Phúc Hợp <hop10102006h@gmail.com>
2025-10-30 07:23:02 +0700

    Delete bài3_11.py

--- bài3_11.py
+++
@@ -1,9 +0,0 @@
-# Dương Phúc Hợp msv: 245752021610047
-def benefit(t, n, k):
-    return n * (1 + t/100) **k
-t = float(input('nhập lãi suất (%/tháng): '))
-n = float(input('nhập số vốn ban đầu: '))
-k = int(input('nhập số tháng gửi: '))
-
-tong_tien = benefit(t, n, k)
-print('số tiền nhận được sau {k} tháng là: ', float(tong_tien))
